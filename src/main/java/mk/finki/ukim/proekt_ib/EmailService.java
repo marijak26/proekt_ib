e9812bb047448c4cb567a9563685a5acaa23c26f
marijak26 <kostadinovikm@gmail.com>
2025-09-18 16:19:19 +0200

    Optimised the code so that it doesn't contain redundant lines and simplified some expressions.

--- src/main/java/mk/finki/ukim/proekt_ib/EmailService.java
+++ src/main/java/mk/finki/ukim/proekt_ib/EmailService.java
@@ -2,7 +2,6 @@ package mk.finki.ukim.proekt_ib;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,14 +15,6 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
-
-    public void sendSimpleMessage(String to, String subject, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        mailSender.send(message);
-    }
 
     public void sendMessageWithAttachment(String to, String subject, String text, File file) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
